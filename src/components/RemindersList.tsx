acba79177233dc0f5487370943064f9048c145e6
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2024-03-25 20:37:41 +0100

    added RemindersList components with interfaces

---
+++ src/components/RemindersList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Reminder {
+  id: number;
+  title: string;
+}
+
+interface ReminderListProps {
+  items: Reminder[];
+}
+
+function RemindersList(props) {
+  return <div></div>;
+}
+
+export default RemindersList;
