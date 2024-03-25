2c078a033819a5bcad56eb01d5352af78c6e0ed5
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2024-03-25 20:38:09 +0100

    button added with classes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-primary"></button>
+      <button className="btn btn-primary">Click me</button>
     </div>
   );
 }
